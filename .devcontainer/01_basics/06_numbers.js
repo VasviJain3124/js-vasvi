2c841a2276a2235fc305b97b13c80bfe7315f317
VasviJain3124 <jainvasvi09@gmail.com>
2025-04-19 16:07:29 +0000

    01_basics config done

---
+++ .devcontainer/01_basics/06_numbers.js
@@ -0,0 +1,24 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(2400)
+console.log(balance);
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber = 123.8967
+console.log(otherNumber.toPrecision(3));
+const hundreds = 1000000000
+console.log(hundreds.toLocaleString('en-IN'));
+
+//+++++++++++++++++++MATH+++++++++++++++++++++++
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(3.6));
+console.log(Math.ceil(4.2));// jo bhi hoga top value hi dega not round off in this case its 5 
+console.log(Math.floor(3.4));//lowest wali value dega here it is 3 
+console.log(Math.min(4,5,6,9,8));
+console.log(Math.max(3,4,5,6,7,));
+console.log(Math.random());
+
+
